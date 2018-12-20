1a19f4241c666533b357c7ed201d510e13f67e16
Kevin Beijer <kevinbeijer1996@gmail.com>
2018-12-20 22:43:38 +0100

    Base
    Base from what I already had

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneManager : MonoBehaviour
+{
+    private string CutsceneName;
+    private string CutscenePath;
+    private int CutsceneNumber;
+
+    private void Start()
+    {
+        CutsceneNumber = 1;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CutsceneNumber++;
+            CutsceneName = "Cutscene_" + CutsceneNumber;
+            CutscenePath = "Sprites/Cutscenes/" + CutsceneName;
+
+            Sprite sp = Resources.Load<Sprite>(CutscenePath);
+            print(sp);
+            GameObject child = transform.GetChild(0).gameObject;
+
+            child.GetComponent<Image>().sprite = sp;
+        }
+    }
+
+    public void NextCutscene()
+    {
+
+    }
+
+    public void SkipCutscene()
+    {
+
+    }
+}
