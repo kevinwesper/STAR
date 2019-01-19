791284662113669370b15da71b95619882228f16
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-01-19 11:20:48 +0100

    save stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,12 @@ public class GameManager : MonoBehaviour
     public static GameManager manager;
 
     #region SaveData
-    public float health;
+    public float playerHealth;
+    public float enemyHealth;
+
+    public int currentScene; // Cutscenes and Battle scenes.
+
+    // https://unity3d.com/learn/tutorials/topics/scripting/persistence-saving-and-loading-data
     #endregion
 
     private void Awake()
@@ -19,7 +24,14 @@ public class GameManager : MonoBehaviour
         }
         else if (manager != this)
         {
-
+            Destroy(gameObject);
         }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 150, 30), "Player health: " + playerHealth);
+        GUI.Label(new Rect(10, 30, 150, 30), "Enemy health: " + enemyHealth);
+        GUI.Label(new Rect(10, 50, 150, 30), "Scene: " + currentScene);
     }
 }
