b6095bcc3272dba04b8656cd54227964dee20af5
Kevin Beijer <kevinbeijer1996@gmail.com>
2019-01-19 11:01:11 +0100

    test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager;
+
+    #region SaveData
+    public float health;
+    #endregion
+
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            manager = this;
+        }
+        else if (manager != this)
+        {
+
+        }
+    }
+}
